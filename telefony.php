4cb91d747a08f847b839cef3916f1b6c4cfef2fc
Deelma <156914350+Deelma@users.noreply.github.com>
2025-02-03 11:23:12 +0100

    telefony zrobione
    nowe zadania

---
+++ telefony.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="pl_PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Telefony</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+</body>
+</html>
+
+<?php
+
+$PDO = new PDO('mysql:host=localhost;dbname=telefony;charset=utf8mb4;port=3306', 'root', '');
+
+$stmt = $PDO->prepare('SELECT * FROM telefon');
+
+$stmt->execute();
+
+echo '<table>';
+
+echo '<tr><th>Marka</th><th>Model</th></tr>';
+
+// ? Foreachem
+
+// foreach($stmt as $row){
+
+//     echo '<tr><td>' . $row['marka'] . '</td><td>' . $row['model'] . '</td></tr>';
+
+// }
+
+
+// ? While'm
+
+$rekordy = 0;
+
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+    $rekordy += 1;
+
+    echo '<tr><td>' . $row['marka'] . '</td><td>' . $row['model'] . '</td></tr>';
+
+}
+
+echo "</table><br>";
+
+echo "W bazie jest tyle telefonów: " . $rekordy;
+
+?>
